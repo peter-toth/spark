610c3c2a18014174b2a9c17739cd5e33f6602bd8
Dongjoon Hyun <dhyun@apple.com>
2019-02-13 16:04:27 -0800

    [SPARK-26865][SQL] DataSourceV2Strategy should push normalized filters
    This PR aims to make `DataSourceV2Strategy` normalize filters like [FileSourceStrategy](https://github.com/apache/spark/blob/master/sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/FileSourceStrategy.scala#L150-L158) when it pushes them into `SupportsPushDownFilters.pushFilters`.
    
    Pass the Jenkins with the newly added test case.
    
    Closes #23770 from dongjoon-hyun/SPARK-26865.
    
    Change-Id: If4de74675e785222fd6eb8f89d8b827f3fc6497f
    Authored-by: Dongjoon Hyun <dhyun@apple.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/DataSourceV2Strategy.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/DataSourceV2Strategy.scala
@@ -20,7 +20,7 @@ package org.apache.spark.sql.execution.datasources.v2
 import scala.collection.mutable
 
 import org.apache.spark.sql.{sources, Strategy}
-import org.apache.spark.sql.catalyst.expressions.{And, AttributeReference, AttributeSet, Expression}
+import org.apache.spark.sql.catalyst.expressions.{And, AttributeReference, AttributeSet, Expression, SubqueryExpression}
 import org.apache.spark.sql.catalyst.planning.PhysicalOperation
 import org.apache.spark.sql.catalyst.plans.logical.{AppendData, LogicalPlan, Repartition}
 import org.apache.spark.sql.execution.{FilterExec, ProjectExec, SparkPlan}
@@ -101,10 +101,13 @@ object DataSourceV2Strategy extends Strategy {
   override def apply(plan: LogicalPlan): Seq[SparkPlan] = plan match {
     case PhysicalOperation(project, filters, relation: DataSourceV2Relation) =>
       val reader = relation.newReader()
+
+      val normalizedFilters = DataSourceStrategy.normalizeFilters(filters, relation.output)
+
       // `pushedFilters` will be pushed down and evaluated in the underlying data sources.
       // `postScanFilters` need to be evaluated after the scan.
       // `postScanFilters` and `pushedFilters` can overlap, e.g. the parquet row group filter.
-      val (pushedFilters, postScanFilters) = pushFilters(reader, filters)
+      val (pushedFilters, postScanFilters) = pushFilters(reader, normalizedFilters)
       val output = pruneColumns(reader, relation, project ++ postScanFilters)
       logInfo(
         s"""

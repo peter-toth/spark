60fb05f7762f92babee82003513223e838f3b583
Marcelo Vanzin <vanzin@cloudera.com>
2019-07-29 15:49:32 -0700

    CDPD-2628. Add pre-commit hook to run Spark unit tests.
    Change-Id: I10ee579eb0658189548e701d26caeca4e372f299

---
+++ cloudera/pre_commit_hook.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# This script (pre_commit_hook.sh) is executed by pre-commit jobs
+#
+# This script is called from inside the spark source code directory, and it
+# is used to build and test the current Spark code.
+#
+
+# -e will make the script exit if an error happens on any command executed
+set -ex
+
+export PATH=${JAVA_HOME}/bin:${PATH}
+
+# To make some of the output quieter
+export AMPLAB_JENKINS=1
+
+MVN_REPO_LOCAL=$HOME/.m2/repository${M2_REPO_SUFFIX}
+
+export MAVEN_OPTS="-XX:ReservedCodeCacheSize=512m"
+
+# Build machines seem to have a CDH-based settings.xml in their maven config directory,
+# which breaks CDPD builds. If that file is found, override it with an empty one.
+if [ -f "$HOME/.m2/settings.xml" ]; then
+  mkdir -p target
+  cat > target/settings.xml <<EOF
+<settings></settings>
+EOF
+  MAVEN_ARGS="-s target/settings.xml"
+else
+  MAVEN_ARGS=""
+fi
+
+export APACHE_MIRROR=http://mirror.infra.cloudera.com/apache
+./build/mvn -B $MAVEN_ARGS -Dcdpd.build=true package -fae -Dmaven.repo.local="$MVN_REPO_LOCAL"

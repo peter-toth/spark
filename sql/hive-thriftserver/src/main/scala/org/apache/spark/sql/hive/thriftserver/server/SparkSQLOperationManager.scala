c4eb8a2d288853754088b8148cb2f0774b9eba24
sychen <sychen@ctrip.com>
2019-09-09 22:06:19 -0700

    [SPARK-26598][SQL] Fix HiveThriftServer2 cannot be modified hiveconf/hivevar variables
    The intent to use the --hiveconf/--hivevar parameter is just an initialization value, so setting it once in ```SparkSQLSessionManager#openSession``` is sufficient, and each time the ```SparkExecuteStatementOperation``` setting causes the variable to not be modified.
    
    It is wrong to set the --hivevar/--hiveconf variable in every ```SparkExecuteStatementOperation```, which prevents variable updates.
    
    ```
    cat <<EOF > test.sql
    select '\${a}', '\${b}';
    set b=bvalue_MOD_VALUE;
    set b;
    EOF
    
    beeline -u jdbc:hive2://localhost:10000 --hiveconf a=avalue --hivevar b=bvalue -f test.sql
    ```
    current result:
    ```
    +-----------------+-----------------+--+
    |     avalue      |     bvalue      |
    +-----------------+-----------------+--+
    | avalue          | bvalue          |
    +-----------------+-----------------+--+
    +-----------------+-----------------+--+
    |       key       |      value      |
    +-----------------+-----------------+--+
    | b               | bvalue          |
    +-----------------+-----------------+--+
    1 row selected (0.022 seconds)
    ```
    after modification:
    ```
    +-----------------+-----------------+--+
    |     avalue      |     bvalue      |
    +-----------------+-----------------+--+
    | avalue          | bvalue          |
    +-----------------+-----------------+--+
    +-----------------+-----------------+--+
    |       key       |      value      |
    +-----------------+-----------------+--+
    | b               | bvalue_MOD_VALUE|
    +-----------------+-----------------+--+
    1 row selected (0.022 seconds)
    ```
    
    modified the existing unit test
    
    Closes #25722 from cxzl25/fix_SPARK-26598.
    
    Authored-by: sychen <sychen@ctrip.com>
    Signed-off-by: Yuming Wang <wgyumg@gmail.com>
    (cherry picked from commit 962e330955581aea032ff336a12f23374c39e672)
    Change-Id: I14768d7535afec85c4ab01bf6ef2224c6fd169d9

--- sql/hive-thriftserver/src/main/scala/org/apache/spark/sql/hive/thriftserver/server/SparkSQLOperationManager.scala
+++ sql/hive-thriftserver/src/main/scala/org/apache/spark/sql/hive/thriftserver/server/SparkSQLOperationManager.scala
@@ -28,7 +28,6 @@ import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.hive.HiveUtils
 import org.apache.spark.sql.hive.thriftserver._
-import org.apache.spark.sql.internal.SQLConf
 
 /**
  * Executes queries using Spark SQL, and maintains a list of handles to active queries.
@@ -51,9 +50,6 @@ private[thriftserver] class SparkSQLOperationManager()
     require(sqlContext != null, s"Session handle: ${parentSession.getSessionHandle} has not been" +
       s" initialized or had already closed.")
     val conf = sqlContext.sessionState.conf
-    val hiveSessionState = parentSession.getSessionState
-    setConfMap(conf, hiveSessionState.getOverriddenConfigurations)
-    setConfMap(conf, hiveSessionState.getHiveVariables)
     val runInBackground = async && conf.getConf(HiveUtils.HIVE_THRIFT_SERVER_ASYNC)
     val operation = new SparkExecuteStatementOperation(parentSession, statement, confOverlay,
       runInBackground)(sqlContext, sessionToActivePool)

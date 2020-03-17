6633767c2848f25bc910667a8a23a9f4ea68c112
Jungtaek Lim (HeartSaVioR) <kabhwan.opensource@gmail.com>
2020-03-17 17:06:03 +0900

    CDPD-8328. [SS] FileStreamSink: Avoid reading the metadata log file when finding the latest batch ID
     ### What changes were proposed in this pull request?
    
    This patch adds the new method `getLatestBatchId()` in CompactibleFileStreamLog in complement of getLatest() which doesn't read the content of the latest batch metadata log file, and apply to `FileStreamSink.addBatch()` to avoid unnecessary latency on reading log file.
    
     ### Why are the changes needed?
    
    Once compacted metadata log file becomes huge, writing outputs for the compact + 1 batch is also affected due to unnecessarily reading the compacted metadata log file. This unnecessary latency can be simply avoided.
    
     ### Does this PR introduce any user-facing change?
    
    No.
    
     ### How was this patch tested?
    
    New UT. Also manually tested under query which has huge metadata log on file stream sink:
    
    > before applying the patch
    
    ![Screen Shot 2020-02-21 at 4 20 19 PM](https://user-images.githubusercontent.com/1317309/75016223-d3ffb180-54cd-11ea-9063-49405943049d.png)
    
    > after applying the patch
    
    ![Screen Shot 2020-02-21 at 4 06 18 PM](https://user-images.githubusercontent.com/1317309/75016220-d235ee00-54cd-11ea-81a7-7c03a43c4db4.png)
    
    Peaks are compact batches - please compare the next batch after compact batches, especially the area of "light brown".
    
    NOTE: This is the preview of SPARK-30915
    
    Change-Id: I29029e6241132d0269aa01f11a5b24ee177032ff

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/CompactibleFileStreamLog.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/CompactibleFileStreamLog.scala
@@ -165,6 +165,26 @@ abstract class CompactibleFileStreamLog[T <: AnyRef : ClassTag](
   }
 
   /**
+   * Return the latest batch Id.
+   *
+   * This method is a complement of getLatest() - while metadata log file per batch tends to be
+   * small, it doesn't apply to the compacted log file. This method only checks for existence of
+   * file to avoid huge cost on reading and deserializing compacted log file.
+   */
+  def getLatestBatchId(): Option[Long] = {
+    val batchIds = fileManager.list(metadataPath, batchFilesFilter)
+      .map(f => pathToBatchId(f.getPath))
+      .sorted(Ordering.Long.reverse)
+    for (batchId <- batchIds) {
+      val batchMetadataFile = batchIdToPath(batchId)
+      if (fileManager.exists(batchMetadataFile)) {
+        return Some(batchId)
+      }
+    }
+    None
+  }
+
+  /**
    * Compacts all logs before `batchId` plus the provided `logs`, and writes them into the
    * corresponding `batchId` file. It will delete expired files as well if enabled.
    */

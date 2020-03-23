0c509012b201f12a9d256dd05774e92c17e73326
Imran Rashid <irashid@cloudera.com>
2020-03-22 21:54:12 -0500

    CDPD-8801.  Fix Nexus syntax for sbt build.
    Change-Id: Ic45af8ba6ad0b1ce251af3aa3545b628fbc38f35

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -230,7 +230,7 @@ object SparkBuild extends PomBuild {
       "gcs-maven-central-mirror" at "https://maven-central.storage-download.googleapis.com/maven2/",
       DefaultMavenRepository,
       Resolver.mavenLocal,
-      Resolver.url("cloudera", url("https://nexus-private.hortonworks.com/nexus/content/groups/public")),
+      "Cloudera Nexus" at "https://nexus-private.hortonworks.com/nexus/content/groups/public",
       Resolver.file("local", file(Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns)
     ),
     externalResolvers := resolvers.value,

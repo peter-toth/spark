a22fec537c4a4257c96757d4ff4a3e22cd4fdc73
“attilapiros” <piros.attila.zsolt@gmail.com>
2021-01-11 14:25:12 -0800

    [SPARK-33711][K8S] Avoid race condition between POD lifecycle manager and scheduler backend
    Missing POD detection is extended by timestamp (and time limit) based check to avoid wrongfully detection of missing POD detection.
    
    The two new timestamps:
    - `fullSnapshotTs` is introduced for the `ExecutorPodsSnapshot` which only updated by the pod polling snapshot source
    - `registrationTs` is introduced for the `ExecutorData` and it is initialized at the executor registration at the scheduler backend
    
    Moreover a new config `spark.kubernetes.executor.missingPodDetectDelta` is used to specify the accepted delta between the two.
    
    Watching a POD (`ExecutorPodsWatchSnapshotSource`) only inform about single POD changes. This could wrongfully lead to detecting of missing PODs (PODs known by scheduler backend but missing from POD snapshots) by the executor POD lifecycle manager.
    
    A key indicator of this error is seeing this log message:
    
    > "The executor with ID [some_id] was not found in the cluster but we didn't get a reason why. Marking the executor as failed. The executor may have been deleted but the driver missed the deletion event."
    
    So one of the problem is running the missing POD detection check even when a single POD is changed without having a full consistent snapshot about all the PODs (see `ExecutorPodsPollingSnapshotSource`).
    The other problem could be the race between the executor POD lifecycle manager and the scheduler backend: so even in case of a having a full snapshot the registration at the scheduler backend could precede the snapshot polling (and processing of those polled snapshots).
    
    Yes. When the POD is missing then the reason message explaining the executor's exit is extended with both timestamps (the polling time and the executor registration time) and even the new config is mentioned.
    
    The existing unit tests are extended.
    
    Closes #30675 from attilapiros/SPARK-33711.
    
    Authored-by: “attilapiros” <piros.attila.zsolt@gmail.com>
    Signed-off-by: Holden Karau <hkarau@apple.com>
    (cherry picked from commit 6bd7a6200f8beaab1c68b2469df05870ea788d49)
    Change-Id: Ib143d8e04c68163964302eebddace4057bff71f9

--- core/src/main/scala/org/apache/spark/scheduler/cluster/ExecutorData.scala
+++ core/src/main/scala/org/apache/spark/scheduler/cluster/ExecutorData.scala
@@ -27,12 +27,14 @@ import org.apache.spark.rpc.{RpcAddress, RpcEndpointRef}
  * @param executorHost The hostname that this executor is running on
  * @param freeCores  The current number of cores available for work on the executor
  * @param totalCores The total number of cores available to the executor
+ * @param registrationTs The registration timestamp of this executor
  */
 private[cluster] class ExecutorData(
-   val executorEndpoint: RpcEndpointRef,
-   val executorAddress: RpcAddress,
-   override val executorHost: String,
-   var freeCores: Int,
-   override val totalCores: Int,
-   override val logUrlMap: Map[String, String]
+    val executorEndpoint: RpcEndpointRef,
+    val executorAddress: RpcAddress,
+    override val executorHost: String,
+    var freeCores: Int,
+    override val totalCores: Int,
+    override val logUrlMap: Map[String, String],
+    val registrationTs: Long
 ) extends ExecutorInfo(executorHost, totalCores, logUrlMap)
